56e28cf5f2b668c975e189108253879783b96228
Zekhnini <ali.zekhnini@student.vinci.be>
2023-11-17 16:54:24 +0100

    Manage null, undefined, and empty values by using a default value

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 
-function greet(names) {
-    if (names === 'Jean-Kevin') return 'Hello, ' + names + '.'
+function greet(name) {
+    if (Array.isArray(name)) {
+        const lowerNames = name.filter(n => n === n.toLowerCase());
+        const upperNames = name.filter(n => n === n.toUpperCase());
+
+        const greetLower = lowerNames.length > 0 ? `Hello, ${lowerNames.join(', ')}. ` : '';
+        const greetUpper = upperNames.length > 0 ? `HELLO, ${upperNames.join(', ')}!` : '';
+
+        return greetLower + greetUpper;
+    }
+
+    if (!name || typeof name !== 'string') {
+        return 'Hello, my friend.';
+    }
+
+    return name.toUpperCase() === name ? `HELLO, ${name}!` : `Hello, ${name}.`;
 }
 
 module.exports = greet
--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,11 @@
 const greet = require('./app')
 
-test('should to be return Hello, Jean-Kevin', () => {
-  const result = greet('Jean-Kevin')
-  expect(result).toEqual('Hello, Jean-Kevin.')
-})
+test('should to be return Hello, (name in table)', () => {
+    const result = greet('Jean-Kevin')
+    expect(result).toEqual('Hello, Jean-Kevin.')
+}),
+    test('should to be return Hello, my friend when array is null', () => {
+        const result = greet(null)
+        expect(result).toEqual('Hello, my friend.')
+    })
+
