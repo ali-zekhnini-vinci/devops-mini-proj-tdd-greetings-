dd52d3cc533f95c3efe87421cd9861329eaf0a6e
Zekhnini <ali.zekhnini@student.vinci.be>
2023-11-17 17:50:39 +0100

    handle multiples names (+2)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
-
 function greet(name) {
+
+    if (name==null || name=="" || name==undefined) {
+        return 'Hello, my friend.';
+    }   
+
+    if (Array.isArray(name)) {
+        if (name.length ==2){
+            return "Hello, "+name[0]+" and "+name[1]+'.';
+        }
+        else{
+            let greet = "Hello, ";
+            for (let index = 0; index < name.length; index++) {
+                greet+= name[i];
+                if(i==name.length-1){
+                    greet+=".";
+                }
+                else if(i==name.length-2){
+                    greet+=" and ";
+
+                }
+                else{
+                    greet+=", ";
+                }
+                
+            }
+            return greet;
+        }
+    }
+    
+    if (name=== name.toUpperCase()){
+        return "HELLO, "+name+'.';
+    }
+
+
 
     return `Hello, ${name}.`;
 }
+
+
 
 module.exports = greet
--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,23 @@
 
 
-const greet = require('./app')
+const greet = require('./app');
 
 test('should to be return Hello, Bob', () => {
     const result = greet('Bob')
     expect(result).toEqual('Hello, Bob.')
-})
+}),
 
+    test('should to be return Hello, my friend when array is null', () => {
+        const result = greet(null)
+        expect(result).toEqual('Hello, my friend.')
+    }),
+
+    test('should to be return HELLO, BOB', () => {
+        const result = greet('BOB')
+        expect(result).toEqual('HELLO, BOB.')
+    }),
+
+    test('should to be return Hello, Bob and Tom', () => {
+        const result = greet(['Bob', 'Tom'])
+        expect(result).toEqual('Hello, Bob and Tom.')
+    })
